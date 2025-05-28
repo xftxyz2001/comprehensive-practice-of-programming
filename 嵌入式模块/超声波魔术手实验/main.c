578b2a9940e923e960028faa2903f93a35af8b01
通晓宇宙 <2581011320@qq.com>
2025-05-28 08:07:24 +0800

    实验报告、互评

--- 嵌入式模块/超声波魔术手实验/main.c
+++ 嵌入式模块/超声波魔术手实验/main.c
@@ -1,5 +1,5 @@
-//连接方式 ：请参考interface.h文件
-//部份源码来源网络--请试用24小时后删除
+// 连接方式 ：请参考interface.h文件
+// 部份源码来源网络--请试用24小时后删除
 #include "stm32f10x.h"
 #include "interface.h"
 #include "LCD1602.h"
@@ -7,26 +7,35 @@
 #include "motor.h"
 #include "UltrasonicCtrol.h"
 
-//全局变量定义
-unsigned int speed_count=0;//占空比计数器 50次一周期
-char front_left_speed_duty=SPEED_DUTY;
-char front_right_speed_duty=SPEED_DUTY;
-char behind_left_speed_duty=SPEED_DUTY;
-char behind_right_speed_duty=SPEED_DUTY;
+// 全局变量定义
+unsigned int speed_count = 0; // 占空比计数器 50次一周期
+char front_left_speed_duty = SPEED_DUTY;
+char front_right_speed_duty = SPEED_DUTY;
+char behind_left_speed_duty = SPEED_DUTY;
+char behind_right_speed_duty = SPEED_DUTY;
 
-unsigned char tick_5ms = 0;//5ms计数器，作为主函数的基本周期
-unsigned char tick_1ms = 0;//1ms计数器，作为电机的基本计数器
-unsigned char tick_200ms = 0;//刷新显示
+unsigned char tick_5ms = 0;	  // 5ms计数器，作为主函数的基本周期
+unsigned char tick_1ms = 0;	  // 1ms计数器，作为电机的基本计数器
+unsigned char tick_200ms = 0; // 刷新显示
 
-char ctrl_comm = COMM_STOP;//控制指令
-char ctrl_comm_last = COMM_STOP;//上一次的指令
-unsigned char continue_time=0;
+char ctrl_comm = COMM_STOP;		 // 控制指令
+char ctrl_comm_last = COMM_STOP; // 上一次的指令
+unsigned char continue_time = 0;
 
 void BarrierProc(void)
 {
-
-	
-	
+	if (distance_cm < 10 || distance_cm > 25)
+	{
+		ctrl_comm = COMM_STOP; // 过近或检测不到手
+	}
+	else if (distance_cm >= 10 && distance_cm <= 15)
+	{
+		ctrl_comm = COMM_DOWN; // 手太近，小车后退
+	}
+	else if (distance_cm > 15 && distance_cm <= 25)
+	{
+		ctrl_comm = COMM_UP; // 手远一些，小车前进跟随
+	}
 }
 
 int main(void)
@@ -41,36 +50,45 @@ int main(void)
 	UltraSoundInit();
 	ServoInit();
 
- while(1)
- {	 
-	 		if(tick_5ms >= 5)
+	while (1)
+	{
+		if (tick_5ms >= 5)
 		{
 			tick_5ms = 0;
 			tick_200ms++;
-			if(tick_200ms >= 40)
+			if (tick_200ms >= 40)
 			{
 				tick_200ms = 0;
 				LEDToggle(LED_PIN);
-				LCD1602WriteDistance(distance_cm);//更新距离
+				LCD1602WriteDistance(distance_cm); // 更新距离
 			}
-			Distance();//计算距离
+			Distance(); // 计算距离
 			BarrierProc();
-			if(ctrl_comm_last != ctrl_comm)
+			if (ctrl_comm_last != ctrl_comm)
 			{
 				ctrl_comm_last = ctrl_comm;
-				switch(ctrl_comm)
+				switch (ctrl_comm)
 				{
-					case COMM_UP:    CarGo();break;
-					case COMM_DOWN:  CarBack();break;
-					case COMM_LEFT:  CarLeft();break;
-					case COMM_RIGHT: CarRight();break;
-					case COMM_STOP:  CarStop();break;
-					default : break;
+				case COMM_UP:
+					CarGo();
+					break;
+				case COMM_DOWN:
+					CarBack();
+					break;
+				case COMM_LEFT:
+					CarLeft();
+					break;
+				case COMM_RIGHT:
+					CarRight();
+					break;
+				case COMM_STOP:
+					CarStop();
+					break;
+				default:
+					break;
 				}
-				//LCD1602WriteCommand(ctrl_comm);
+				// LCD1602WriteCommand(ctrl_comm);
 			}
 		}
-		
- }
+	}
 }
-
